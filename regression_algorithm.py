64402d4dc1a54c34dd09c60d9fa44eb6ee4eaf27
mkhs <mkh_sharif@aol.com>
2019-01-20 13:51:14 -0500

    add intercept calculation

--- regression_algorithm.py
+++ regression_algorithm.py
@@ -5,11 +5,13 @@ import matplotlib.pyplot as plt
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,4,6,7], dtype=np.float64)
 
-# generate the best fit slope
-def best_fit_slope(xs,ys):
+# generate the best fit slope and intercept
+def best_fit_slope_and_intercept(xs,ys):
     m = (((mean(xs) * mean(ys)) - mean(xs*ys)) / ((mean(xs)**2) - mean(xs**2)))
-    return m
+    b = mean(ys) - m*mean(xs)
+    return m, b
 
-m  = best_fit_slope(xs,ys)
-print(m)
+m, b  = best_fit_slope_and_intercept(xs,ys)
+print(m,b)
+
 
