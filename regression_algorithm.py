27adade66c858fd58626d09a4fe5969068c5ed24
mkhs <mkh_sharif@aol.com>
2019-01-20 14:12:11 -0500

    add notes

--- regression_algorithm.py
+++ regression_algorithm.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 style.use('fivethirtyeight')
 
+'''
+Linear Regression Algorithm 
+Steps:
+1. Gather Data
+2. Fit Model to Data 
+3. Make prediction on Data
+'''
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,4,6,7], dtype=np.float64)
 
@@ -19,6 +26,7 @@ print(m,b)
 # create regression line
 regression_line = [(m*x)+b for x in xs]
 
+# make prediction
 predict_x = 8
 predict_y = (m*predict_x)+b
 
